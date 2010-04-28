a70fecbc0946b6a872bea3d5c0ba6a1b91ac5d6b
Stephen Hardisty <shardisty@administrators-macbook-pro-8.local>
2010-04-28 21:17:10 +1000

    added .gitignore and rectified dodgy step

--- features/steps/when_i_go_to_steps.rb
+++ features/steps/when_i_go_to_steps.rb
@@ -1,7 +1,7 @@
 When /^I drive my car to "([^\"]*)"$/ do |location|
-  @vehicle.location = location
+  @vehicle.drive_to location
 end
 
-When /^I fly my plane to "([^\"]*)"$/ do |arg1|
-  @vehicle.location = location
+When /^I fly my plane to "([^\"]*)"$/ do |location|
+  @vehicle.fly_to location
 end
